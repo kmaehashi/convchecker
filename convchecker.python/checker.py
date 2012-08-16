d596ef69f12261a5a385b0bf7d633e18221bc255
ODA Satoshi <oda.satoshi@gmail.com>
2012-08-16 10:16:03 +0900

    how to use bulk_query

--- convchecker.python/checker.py
+++ convchecker.python/checker.py
@@ -5,7 +5,7 @@ import sys,json
 import convchecker.client
 import convchecker.types
 
-NAME = "stat_cla";
+NAME = "checker";
 
 if __name__ == '__main__':
 
@@ -15,8 +15,13 @@ if __name__ == '__main__':
   config = convchecker.types.config_data(json.dumps(converter))
   checker.set_config(NAME, config)
 
-  datum = convchecker.types.datum(  [["URL", "We love hack!"]], [["weight", 10.0]] )
-  print checker.query(NAME, datum)
+  datum0 = convchecker.types.datum(  [["message", "We love jubatus!"]], [["rank", 10.0]] )
+  datum1 = convchecker.types.datum(  [["name", "jubatus"],["license", "LGPL v2.1"],["author", "NTT & PFI"]], [["version", 0.31]] )
+  datum2 = convchecker.types.datum(  [["URL", "http://jubat.us/"]], [["count", 256.0],["lenght", 10243.0]] )
+  
+  print checker.query(NAME, datum0)
+  print "--------------------------------------"
+  print checker.bulk_query(NAME, [datum1,datum2])
 
 
 
