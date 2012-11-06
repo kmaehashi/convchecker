54469d3df11de4d86b8d7291bf95f290642f2870
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2012-11-06 15:47:31 +0900

    fix for Jubatus 0.3.3

--- convchecker_impl.cpp
+++ convchecker_impl.cpp
@@ -2,6 +2,7 @@
 #include <jubatus/framework.hpp>
 #include "convchecker_server.hpp"
 #include "convchecker_serv.hpp"
+
 using namespace jubatus;
 using namespace jubatus::framework;
 namespace jubatus { namespace server {
@@ -10,38 +11,38 @@ class convchecker_impl_ : public convchecker<convchecker_impl_>
 public:
   convchecker_impl_(const server_argv& a):
     convchecker<convchecker_impl_>(a.timeout),
-    p_(new convchecker_serv(a))
+    p_(new server_helper<convchecker_serv>(a))
   {}
 
   bool set_config(std::string name, config_data c) //update broadcast
-  { JWLOCK__(p_); return p_->set_config(c); }
+  { JWLOCK__(p_); return get_p()->set_config(c); }
 
   config_data get_config(std::string name) //analysis random
-  { JRLOCK__(p_); return p_->get_config(); }
+  { JRLOCK__(p_); return get_p()->get_config(); }
 
   std::string query(std::string name, datum query) //update random
-  { JWLOCK__(p_); return p_->query(query); }
+  { JWLOCK__(p_); return get_p()->query(query); }
 
   std::string bulk_query(std::string name, std::vector<datum > query) //update random
-  { JWLOCK__(p_); return p_->bulk_query(query); }
+  { JWLOCK__(p_); return get_p()->bulk_query(query); }
 
   bool save(std::string name, std::string id) //update broadcast
-  { JWLOCK__(p_); return p_->save(id); }
+  { JWLOCK__(p_); return get_p()->save(id); }
 
   bool load(std::string name, std::string id) //update broadcast
-  { JWLOCK__(p_); return p_->load(id); }
+  { JWLOCK__(p_); return get_p()->load(id); }
 
   std::map<std::string,std::map<std::string,std::string > > get_status(std::string name) //analysis broadcast
   { JRLOCK__(p_); return p_->get_status(); }
   int run(){ return p_->start(*this); };
-  common::cshared_ptr<convchecker_serv> get_p(){ return p_; };
+  common::cshared_ptr<convchecker_serv> get_p(){ return p_->server(); };
 private:
-  common::cshared_ptr<convchecker_serv> p_;
+  common::cshared_ptr<server_helper<convchecker_serv> > p_;
 };
 }} // namespace jubatus::server
 int main(int args, char** argv){
   return
     jubatus::framework::run_server<jubatus::server::convchecker_impl_,
                                    jubatus::server::convchecker_serv>
-       (args, argv);
+       (args, argv, "convchecker");
 }
--- wscript
+++ wscript
@@ -1,4 +1,5 @@
 name='convchecker'
+JUBATUS_VERION='0.3.3'
 
 def options(opt):
   opt.load('compiler_cxx')
@@ -6,6 +7,8 @@ def options(opt):
 def configure(conf):
   conf.env.CXXFLAGS += ['-O2', '-Wall', '-g', '-pipe']
   conf.load('compiler_cxx')
+
+  conf.define('JUBATUS_VERSION', JUBATUS_VERION)
 
   conf.check_cxx(lib = 'msgpack', mandatory = True)
   conf.check_cxx(lib = 'glog', mandatory = True)
@@ -20,12 +23,13 @@ def configure(conf):
   conf.check_cxx(lib = 'jubacommon', mandatory = True)
   conf.check_cxx(lib = 'jubacommon_mprpc', mandatory = True)
   conf.check_cxx(lib = 'jubaconverter', mandatory = True)
+  conf.check_cxx(lib = 'jubamixer', mandatory = True)
 
 def build(bld):
   bld.program(
     source = name+'_serv.cpp '+ name +'_impl.cpp',
     target = 'juba' + name,
-    use = ['JUBATUS_FRAMEWORK', 'JUBACOMMON', 'JUBACOMMON_MPRPC', 'JUBACONVERTER', 'PFICOMMON', 'PFICOMMON_CONCURRENT',
+    use = ['JUBATUS_FRAMEWORK', 'JUBACOMMON', 'JUBACOMMON_MPRPC', 'JUBACONVERTER', 'JUBAMIXER', 'PFICOMMON', 'PFICOMMON_CONCURRENT',
            'PFICOMMON_NETWORK_MPRPC', 'PFICOMMON_SYSTEM', 'MSGPACK',  
            'GLOG']
     )
